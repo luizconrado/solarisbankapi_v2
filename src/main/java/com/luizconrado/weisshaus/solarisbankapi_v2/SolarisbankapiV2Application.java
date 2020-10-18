a5f4986e80ec9eb3ee8936bd6e8218c5e4634d11
Luiz Conrado <lfconrado@gmail.com>
2020-10-18 14:07:26 +0200

    Initial Commit

---
+++ src/main/java/com/luizconrado/weisshaus/solarisbankapi_v2/SolarisbankapiV2Application.java
@@ -0,0 +1,13 @@
+package com.luizconrado.weisshaus.solarisbankapi_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarisbankapiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarisbankapiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/luizconrado/weisshaus/solarisbankapi_v2/SolarisbankapiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizconrado.weisshaus.solarisbankapi_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarisbankapiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
