80a092dfb84b2d22c0dea7fbdcec38c9231085bd
Luiz Felipe Conrado <lfconrado@gmail.com>
2020-12-07 04:37:31 +0100

    2020-12-07 04:37

---
+++ src/main/java/com/luizconrado/weisshaus/solarisbankapi_v2/services/Auth0Authentication.java
@@ -0,0 +1,16 @@
+package com.luizconrado.weisshaus.solarisbankapi_v2.services;
+
+public class Auth0Authentication {
+
+    /*
+
+    curl --request POST \
+  --url 'https://YOUR_DOMAIN/oauth/token' \
+  --header 'content-type: application/x-www-form-urlencoded' \
+  --data grant_type=client_credentials \
+  --data 'client_id=YOUR_CLIENT_ID' \
+  --data client_secret=YOUR_CLIENT_SECRET \
+  --data 'audience=https://YOUR_DOMAIN/api/v2/'
+
+     */
+}
